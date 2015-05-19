c80bfe8066b722c87f85515ffcda18068134e44e
KapSteR <hr.kasper.nielsen@gmail.com>
2015-05-19 18:20:12 +0200

    Final commit on hand in 2

--- Exercise2/Exercise7/core/exercise7/exercise7.sdk/exercise7/Debug/src/main.d
+++ Exercise2/Exercise7/core/exercise7/exercise7.sdk/exercise7/Debug/src/main.d
@@ -1,7 +1,40 @@
 src/main.o src/main.d: ../src/main.c \
  ../../exercise7_bsp_2/ps7_cortexa9_0/include/xparameters.h \
- ../../exercise7_bsp_2/ps7_cortexa9_0/include/xparameters_ps.h
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xiosc.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_types.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_assert.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xstatus.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_io.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_printf.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xparameters.h \
+ ../../exercise7_bsp_2/ps7_cortexa9_0/include/xiosc_hw.h
 
 ../../exercise7_bsp_2/ps7_cortexa9_0/include/xparameters.h:
 
 ../../exercise7_bsp_2/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xiosc.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_types.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_assert.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xstatus.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_io.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xil_printf.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xparameters.h:
+
+../../exercise7_bsp_2/ps7_cortexa9_0/include/xiosc_hw.h:
